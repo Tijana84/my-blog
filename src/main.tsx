77f98b4ad2eb306582b63dca644b83f92f5a929b
Tijana Savic <tijanasavic1984@gmail.com>
2023-10-25 16:22:34 +0200

    added home page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Hero } from "./pages/Hero";
+import { Home } from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Hero />}></Route>
+        <Route path="/home" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
